f8f5cd7aa22be55699f927c94c75baca3317e925
arthur273 <arthur.lacava01@gmail.com>
2024-11-05 21:10:40 -0300

    deleting makefile tests

--- src/Game.cpp
+++ src/Game.cpp
@@ -81,7 +81,7 @@ void Game::INIT_SDL() {
     }
 
 
-    // Initialize SDL_mixer
+    // Testando se posso fazer isso aqui
     if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0) {
         SDL_Log("SDL_mixer could not initialize! SDL_mixer Error: %s", Mix_GetError());
     }
